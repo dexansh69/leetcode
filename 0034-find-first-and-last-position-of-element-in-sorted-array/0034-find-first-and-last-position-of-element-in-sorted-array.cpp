d49524559aacf4fc271f5b1762381d552deacbe6
dexansh69 <96936759+dexansh69@users.noreply.github.com>
2026-07-28 09:26:48 +0530

    Time: 0 ms (100%), Space: 17.7 MB (20.39%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -3,42 +3,41 @@ public:
     vector<int> searchRange(vector<int>& nums, int target) {
         int n = nums.size();
         int left = 0;
-        int right = n-1;
-        int first_occurence=-1 ;
-        int last_occurence=-1;
-        while(left<=right){
-            int mid = (left+right)/2;
-            if(nums[mid]==target){
-                first_occurence=mid;
-                right = mid-1;
-            }
-            else if(nums[mid]>target){
-                right = mid-1;
+        int right = n - 1;
+        int first = -1;
+        int last = -1;
+        vector<int> ans;
+        if (nums.empty())
+            return {-1, -1};
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] == target) {
+                first = mid;
+                right = mid - 1;
+            } else if (nums[mid] > target) {
 
-            }
-            else if (nums[mid]<target){
-                left = mid +1;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
             }
         }
-        left=0;
-        right = n-1;
-        while(left<=right){
-            int mid = (left+right)/2;
-            if(nums[mid]==target){
-                last_occurence=mid;
-                left = mid+1;
-            }
-            else if(nums[mid]>target){
-                right = mid-1;
+        left = 0;
+        right = n - 1;
+        int mid;
+        while (left <= right) {
+            mid = (left + right) / 2;
+            if (nums[mid] == target) {
+                last = mid;
+                left = mid + 1;
+            } else if (nums[mid] > target) {
 
-            }
-            else if (nums[mid]<target){
-                left = mid +1;
+                right = mid - 1;
+            } else {
+
+                left = mid + 1;
             }
         }
-        if(first_occurence ==-1 && last_occurence==-1){
-            return {-1,-1};
-        }
-        return {first_occurence,last_occurence};
+
+        return {first, last};
     }
 };
